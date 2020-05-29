d37d90af0f26bf24a9f53b9aabab675705696db9
tiz <timdaurelle@gmail.com>
2020-05-29 18:12:37 +0200

    add getpop

---
+++ script/getPop.py
@@ -0,0 +1,20 @@
+# importing the requests library 
+import requests 
+import csv
+
+# api-endpoint 
+URL = 'http://geo.api.gouv.fr/communes'#16102
+ 
+r = requests.get(url = URL )
+
+# extracting data in json format 
+data = r.json() 
+header = ["code_insee","nom","population"]
+file_carre = open('tables/getPop.csv', 'w')
+csv_carre_writer = csv.writer(file_carre, delimiter=';')
+csv_carre_writer.writerow(header)
+for item in data:
+    if("population" in item):
+        csv_carre_writer.writerow([item["code"], item["nom"], item["population"]])
+
+
