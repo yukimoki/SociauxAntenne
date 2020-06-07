efcd881af776d88bb6b850b25cdb01ae3947d8d9
Lucas Romanet <lucas@romanet.dev>
2020-06-07 11:28:22 +0200

    arguments plot_chart()

--- script/diagrammes/bar.py
+++ script/diagrammes/bar.py
@@ -1,3 +1,4 @@
+import csv
 import pandas as pd
 import matplotlib.pyplot as plot
 import numpy as np
@@ -5,41 +6,51 @@ import math
 import time
 from progress.bar import IncrementalBar
 
+#Path
 cAll = 'data/carresALL[essai]avecSup.csv'
 cStats = 'data/carres-stats-avec-nb-antennes'
-with open(cAll) as carre_file:
-    cAll = pd.read_csv(carre_file, sep=';', quotechar="'")
-    bar = IncrementalBar('Chargement', max = len(cAll))
-with open(cStats) as carre_file:
-    cStats = pd.read_csv(carre_file, sep='\s+')
-    header = list(cStats.columns.values)
+cEmetteur = 'data/EMETTEUR.csv'
 
-def plot_chart(detail=10, stat='poptot', type='LTE'):
+#File read
+with open(cAll) as carre_file:
+    cAll_df = pd.read_csv(carre_file, sep=';', quotechar="'")
+with open(cStats) as carre_file:
+    cStats_df = pd.read_csv(carre_file, sep='\s+')
+    header = list(cStats_df.columns.values)
+with open(cEmetteur) as emetteur_file:
+    cEmetteur_df = pd.read_csv(emetteur_file, sep=';', quotechar="'")
+
+#Function
+def plot_chart(detail=10, stat='poptot', threshold=0, type='LTE'):
     """
-    in: detail=nombre de barres, stat=statistique INSEE à évaluer, type=technologie de l'antenne
+    in: detail=échantillon (nombre de barres), stat=statistique INSEE à évaluer, threshold=valeur de stat minimum pour être retenue (si négative, maximum), type=technologie de l'antenne
     out: graphique panda associé
     """
+    bar = IncrementalBar('Génération du diagramme', max=len(cAll_df))
     data = []
     stat_index = header.index(stat)
     interval = 17000/detail # 17km = max search dist
     
     for i in range (detail):
         data.append([str(math.floor(i*interval))+' à '+str(math.floor((i+1)*interval-1)), 0])
-    for index, row in cAll.iterrows():
+    for index, row in cAll_df.iterrows():
         bar.next()
+        carre = cStats_df[cStats_df['LAEA']==row['IDcrs']]
         for i in range (1,4): # 4 points du carré
             dist = int(row['SupProchePt'+str(i)].split('-')[1]) # distance de l'antenne au point
-            pop = cStats.loc[cStats['LAEA']==row['IDcrs']].values[0][stat_index] / 4 #values[0][2] => poptot
-            data[math.floor(dist//interval)][1]+=pop
+            pop = carre.values[0][2] / 4 #values[0][2] => poptot
+            if (carre.values[0][stat_index] >= threshold):
+                data[math.floor(dist//interval)][1]+=pop
     bar.finish()
-    df = pd.DataFrame(data,columns=['pop','support'],dtype=float)
+    df = pd.DataFrame(data, columns=['pop','support'], dtype=float)
     print(df)
     df.plot.bar(x="pop", y="support", rot=25, title="Distances des supports les plus proches aux carrés de population")
-    plot.savefig('statpop.png')
-    #plot.show()
 
-plot_chart()
-
-
+#Main
+start_time = time.time()
+plot_chart(detail=11)
+print("Temps d'exécution: %s secondes" % (time.time() - start_time))
+plot.savefig('statpop.png')
+plot.show()
 
 # distribution: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.plot.hist.html
