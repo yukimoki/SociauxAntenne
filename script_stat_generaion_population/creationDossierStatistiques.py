f3cd3a548d731921c243e2a0842bc5f0427b0b20
hadestructhor <hadestructhor@users.noreply.github.com>
2020-06-09 22:05:06 +0200

    modification du script de génération des deux premiers graphs global pour rendre la modification des abscisses  plus facile

--- script_stat_generaion_population/creationDossierStatistiques.py
+++ script_stat_generaion_population/creationDossierStatistiques.py
@@ -6,11 +6,11 @@ if not os.path.exists('../statistiques') :
 if not os.path.exists('../statistiques/emetteur_population_support'):
 	os.mkdir('../statistiques/emetteur_population_support')
 
-if not os.path.exists('../statistiques/emetteur_population_support'):
+if not os.path.exists('../statistiques/emetteur_population_support/StatToutOperateurConfondu'):
 	os.mkdir('../statistiques/emetteur_population_support/StatToutOperateurConfondu')
 
-if not os.path.exists('../statistiques/emetteur_population_support'):
+if not os.path.exists('../statistiques/emetteur_population_support/StatParOperateur'):
 	os.mkdir('../statistiques/emetteur_population_support/StatParOperateur')
 
-if not os.path.exists('../statistiques/emetteur_population_support'):
+if not os.path.exists('../statistiques/emetteur_population_support/StatParOperateurMemeOrdonne'):
 	os.mkdir('../statistiques/emetteur_population_support/StatParOperateurMemeOrdonne')
