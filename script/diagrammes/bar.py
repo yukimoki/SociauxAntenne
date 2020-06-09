2f2914da3f281063a12f7642f8506c8338dadf04
Lucas Romanet <lucas@romanet.dev>
2020-06-09 17:25:52 +0200

    Fix population manquante

--- script/diagrammes/bar.py
+++ script/diagrammes/bar.py
@@ -1,5 +1,6 @@
 import os
 import csv
+import math
 import pandas as pd
 import matplotlib.pyplot as plot
 import numpy as np
@@ -8,7 +9,7 @@ from progress.bar import IncrementalBar
 
 # Paths
 cAll = 'data/CarresDistSupProche.csv'
-cStats = 'data/carres-stats-avec-nb-antennes'
+cStats = 'data/StatsSocioCarres.csv'
 
 # Files
 print('Ouverture des fichiers...')
@@ -20,7 +21,7 @@ with open(cStats) as carre_file:
     print('\t-Statistiques INSEE')
 
 # Functions
-def plot_chart(detail=10, stat='poptot', threshold=0, tech='ALL', max_search_dist=17000):
+def genData(detail=10, stat='poptot', threshold=0, tech='ALL', max_search_dist=17000):
     """
     in: detail=échantillon (nombre de barres), 
         stat=statistique INSEE à évaluer, 
@@ -37,7 +38,6 @@ def plot_chart(detail=10, stat='poptot', threshold=0, tech='ALL', max_search_dis
                     return True
             return False
         return True
-
     data = []
     interval = max_search_dist/detail # 17km = distance max au support possible
     msd = round((detail*interval)/1000, 2)
@@ -45,40 +45,44 @@ def plot_chart(detail=10, stat='poptot', threshold=0, tech='ALL', max_search_dis
     if (negative_threshold):
         threshold*=-1
     for i in range (detail):
-        data.append([str(round((i*interval)/1000, 2))+' à '+str(round(((i+1)*interval-1)/1000, 2)), 0])
+        data.append([round(((i+1)*interval-1)/1000, 2), 0])
     print('Jointure des fichiers...')
     carres_df = cAll_df.join(cStats_df.set_index('LAEA'), on='IDcrs')
     bar = IncrementalBar('Génération des données', max=len(cAll_df))
     for index, row in carres_df.iterrows():
         bar.next()
-        if (_verify_tech(row)):
-            for i in range (1,4): # 4 points du carré
-                dist = int(row['DistSupProchePt'+str(i)]) # distance de l'antenne au point      
-                pop = row['poptot'] / 4
-                statpc = row[stat]
-                if ((statpc>=threshold and not negative_threshold) or (statpc<=threshold and negative_threshold)):
-                    if (dist//interval < len(data)):
-                        data[round(dist//interval)][1]+=pop
+        pop = row['poptot'] / 4
+        statpc = row[stat]
+        #if (_verify_tech(row)):
+        for i in range (1,5): # 4 points du carré
+            dist = int(row['DistSupProchePt'+str(i)]) # distance de l'antenne au point
+            if ((statpc>=threshold and not negative_threshold) or (statpc<=threshold and negative_threshold)):
+                if (dist/interval < len(data)):
+                    data[math.floor(dist/interval)][1]+=pop
+                else:
+                    if (data[-1][0] == 'Plus de '+str(msd)):
+                        data[-1][1]+=pop
                     else:
-                        if (data[-1][0] == 'Plus de '+str(msd)):
-                            data[-1][1]+=pop
-                        else:
-                            data.append(['Plus de '+str(msd), 0])
-
+                        data.append(['Plus de '+str(msd), 0])
+    
     bar.finish()
-    df = pd.DataFrame(data, columns=['Distance (km)','Population'], dtype=float)
-    print(df)
-    df.to_csv(r'statpop250m.csv', sep='\t', encoding='utf-16')
+    return(pd.DataFrame(data, columns=['Distance max (km)','Population'], dtype=float))
+
+def plotchart(df):
+    plot.savefig('statpop.pdf')
     df.plot.bar(x="Distance (km)", y="Population", rot=25, title="Distances des supports les plus proches aux carrés de population")
     plot.xlabel("Distance au support le plus proche (km)")
     plot.ylabel("Volume total de population")
+    plot.show()
+    
 
 # Main
 
 start_time = time.time()
-plot_chart(detail=25, stat='poptot', threshold=0, tech='ALL', max_search_dist=10000)
-#plot.savefig('statpop.pdf')
+df = genData(detail=40, stat='pcmenagespauvres', threshold=0.1, tech='ALL', max_search_dist=10000)
+print(df)
+df.to_csv(r'pcmenagespauvres_20pc250m.csv', sep='\t', encoding='utf-16', index=False)
 print("Temps d'exécution: %s secondes" % (time.time() - start_time))
-plot.show()
+#plotchart(df)
 
 # distribution: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.plot.hist.html
